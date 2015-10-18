879eb8c3e05924ab58d1169c0dddc3db024f742a
Ulden <manjuulden@hotmail.com>
2015-10-18 14:00:36 +0800

    the "put in db" part is finished, but many bugs found in the zhihu.py need to be fixed

--- test.py
+++ test.py
@@ -5,60 +5,79 @@ from zhihu import User
 from zhihu import Answer
 from zhihu import Search
 
-def question_test(url):
-    question = Question(url)
+def user_test(user_url):
+    user = User(user_url)
+    # 获取用户ID
+    user_id = user.get_user_id()
+    # 获取该用户的关注者人数
+    followers_num = user.get_followers_num()
+    # 获取该用户关注的人数
+    followees_num = user.get_followees_num()
+    # 获取该用户提问的个数
+    asks_num = user.get_asks_num()
+    # 获取该用户回答的个数
+    answers_num = user.get_answers_num()
+    # 获取该用户收藏夹个数
+    collections_num = user.get_collections_num()
+    # 获取该用户获得的赞同数
+    agree_num = user.get_agree_num()
+    # 获取该用户获得的感谢数
+    thanks_num = user.get_thanks_num()
 
-    # 获取该问题的标题
-    title = question.get_title()
-    # 获取该问题的详细描述
-    detail = question.get_detail()
-    # 获取回答个数
-    answers_num = question.get_answers_num()
-    # 获取关注该问题的人数
-    followers_num = question.get_followers_num()
-    # 获取该问题所属话题
-    topics = question.get_topics()
-    # 获取该问题被浏览次数
-    visit_times = question.get_visit_times()
-    # 获取排名第一的回答
-    top_answer = question.get_top_answer()
-    # 获取排名前十的十个回答
-    top_answers = question.get_top_i_answers(10)
-    # 获取所有回答
-    answers = question.get_all_answers()
+    # 获取该用户关注的人
+    followees = user.get_followees()
+    # 获取关注该用户的人
+    followers = user.get_followers()
+    # 获取该用户提的问题
+    asks = user.get_asks()
+    # 获取该用户回答的问题的答案
+    answers = user.get_answers()
+    # 获取该用户的收藏夹
+    collections = user.get_collections()
 
-    print title  # 输出：现实可以有多美好？
-    print detail
-    # 输出：
-    # 本问题相对于“现实可以多残酷？传送门：现实可以有多残酷？
-    # 题主：       昨天看了“现实可以有多残酷“。感觉不太好，所以我
-    # 开了这个问题以相对应，希望能够“中和一下“。和那个问题题主不想
-    # 把它变成“比惨大会“一样，我也不想把这个变成“鸡汤故事会“，或者
-    # 是“晒幸福“比赛。所以大家从“现实，实际”的角度出发，讲述自己的
-    # 美好故事，让大家看看社会的冷和暖，能更加辨证地看待世界，是此
-    # 题和彼题共同的“心愿“吧。
-    print answers_num  # 输出：2441
-    print followers_num  # 输出：26910
-    for topic in topics:
-        print topic,  # 输出：情感克制 现实 社会 个人经历
-    print visit_times  # 输出: 该问题当前被浏览的次数
-    print top_answer  # 输出：<zhihu.Answer instance at 0x7f8b6582d0e0>(Answer类对象)
-    print top_answers  # 输出：<generator object get_top_i_answers at 0x7fed676eb320>(代表前十的Answer的生成器)
+    print user_id  # 黄继新
+    print followers_num  # 614840
+    print followees_num  # 8408
+    print asks_num  # 1323
+    print answers_num  # 786
+    print collections_num  # 44
+    print agree_num  # 46387
+    print thanks_num  # 11477
+
+    print followees
+    # <generator object get_followee at 0x7ffcac3af050>
+    # 代表所有该用户关注的人的生成器对象
+    i = 0
+    for followee in followees:
+        print followee.get_user_id()
+        i = i + 1
+        if i == 41:
+            break
+
+    print followers
+    # <generator object get_follower at 0x7ffcac3af0f0>
+    # 代表所有关注该用户的人的生成器对象
+    i = 0
+    for follower in followers:
+        print follower.get_user_id()
+        i = i + 1
+        if i == 41:
+            break
+
+    print asks
+    # <generator object get_ask at 0x7ffcab9db780>
+    # 代表该用户提的所有问题的生成器对象
     print answers
+    # <generator object get_answer at 0x7ffcab9db7d0>
+    # 代表该用户回答的所有问题的答案的生成器对象
+    print collections
+    # <generator object get_collection at 0x7ffcab9db820>
+    # 代表该用户收藏夹的生成器对象
 
-def searchTest(keyword, search_type):
-    search = Search(keyword, search_type)
-    if search_type is 'question':
-        for question_url in search.getQuestions():
-            question_test(question_url)
-    elif search_type is 'people':
-        peoples = search.getPeoples()
-    elif search_type is 'topic':
-        topics = search.getTopics()
 
 def main():
-    keyword = '屠呦呦'
-    searchTest(keyword,'question')
+    user_url = "http://www.zhihu.com/people/jixin"
+    user_test(user_url)
 
 if __name__ == '__main__':
     main()
--- worm.py
+++ worm.py
@@ -34,9 +34,11 @@ class PutInDB:
         followers_num = question.get_followers_num()
         topics = question.get_topics()
         #answers = question.get_all_answers()
-        cursor.execute("insert into Questions values(%s, %s, %s, %d, %d)" % id,'0000000000' , detail, title, answer_num, followers_num)
+        cursor.execute("insert into Questions values(%s, %s, %s, %d, %d)" % id,'00000000' , detail, title, answer_num, followers_num)
         for topic in topics:
             cursor.execute("instert into topic values(%s)" % topic)
+        cursor.close()
+        self.connect.commit()
 
     def put_answer_in_db(self, answer):
         cursor = self.connect.cursor()
@@ -48,8 +50,46 @@ class PutInDB:
         detail = answer.get_content()
         upvote = answer.get_upvote()
         visit_times = answer.get_visit_times()
-        cursor.execute("inser in answers values(%s, %s, %s, %s, %d, %d)" % id, question_id, author_id, detail, upvote,visit_times)
+        cursor.execute("insert into answers values(%s, %s, %s, %s, %d, %d)" % id, question_id, author_id, detail, upvote,visit_times)
+        cursor.close()
+        self.connect.commit()
 
+    def put_user_in_db(self, user):
+        cursor = self.connect.cursor()
+        user_id = user.get_user_id()
+        follower_num = user.get_followers_num()
+        followee_num = user.get_followees_num()
+        agree_num = user.get_agree_num()
+        thanks_num = user.get_thanks_num()
+        ask_num = user.get_asks_num()
+        answer_num = user.get_answers_num()
+        collection_num = user.get_collections_num()
+        cursor.execute("insert into users values(%s, %d, %d, %d, %d, %d, %d)" % user_id, follower_num, followee_num, agree_num, thanks_num, ask_num, answer_num,collection_num)
 
-    #to be continue~~~
-    #def put_user_in_db(self, user):
+        # for the Tables besides User
+        asks = user.get_asks()
+        followers = user.get_followers()
+        collections = user.get_collections()
+        for question in asks:
+            quesiton_id = question.get_question_id()
+            cursor.execute("update questions set asker_ID=%s where question_id=%d"% user_id, quesiton_id)
+        for user in followers:
+            follower_id = user.get_user_id()
+            cursor.execute("insert into follow_user values(%s, %s)" % follower_id, user_id)
+        for collection in collections:
+            collection_name = collection.get_name()
+            cursor.execute("insert into collection value(%s, %s)" % collection_name, user_id)
+        cursor.close()
+        self.connect.commit()
+
+    def put_collection_in_db(self, collection):
+        cursor = self.connect.cursor()
+        name = collection.get_name()
+        answers = collection.get_all_answers()
+        for answer in answers:
+            answer_id = answer.get_answer_id()
+            cursor.execute("insert into Collection_Answers values(%s, %s)" % name, answer_id)
+        cursor.close()
+        self.connect.commit()
+
+    
